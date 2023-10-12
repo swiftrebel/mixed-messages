39192c3ed1d8ca27b64357a0e6faa73ff668f2d4
David Daniel <davidaudesirk@gmail.com>
2023-10-12 16:06:00 -0500

    store message components

---
+++ mixed-message.js
@@ -0,0 +1,9 @@
+const messages = [
+"We are one with the universe.", 
+"Life is full of possibilities. Time to embrace them!", 
+"Sun, moon, and planets guide me.",
+"Be the person your sun sign says you can be.", 
+"Stars change, and so can we.",
+"It's always a good day to map your birth chart."
+];
+
